e38e0d98722148f5d100da2dd3829a30447dc50f
otabat <otabat@gmail.com>
2014-02-11 01:37:20 +0900

    Updated library version and dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject couchbase-clj "0.1.2"
+(defproject couchbase-clj "0.1.3-SNAPSHOT"
   :description "A Clojure client for Couchbase Server 2.0."
   :url "https://github.com/otabat/couchbase-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.1"]
-                 [couchbase/couchbase-client "1.1.5"]
-                 [spy/spymemcached "2.8.12"]]
-  :plugins [[codox "0.6.4"]]
-  :repositories {"couchbase" {:url "http://files.couchbase.com/maven2"}})
+                 [org.clojure/data.json "0.2.4"]
+                 [com.couchbase.client/couchbase-client "1.3.2"]]
+  :plugins [[codox "0.6.4"]])
